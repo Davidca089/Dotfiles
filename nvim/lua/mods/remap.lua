441b3aa733dfa36ed7ae3bc5f2fe8597851b5988
David <david-ortiz.calderon@epita.fr>
2023-01-10 18:29:15 -0500

    Fixed nvim config for newer version

--- nvim/lua/mods/remap.lua
+++ nvim/lua/mods/remap.lua
@@ -1,46 +1,46 @@
-local Remap = require("mods.keymap")
-local nnoremap = Remap.nnoremap
-local vnoremap = Remap.vnoremap
-local inoremap = Remap.inoremap
-local xnoremap = Remap.xnoremap
+vim.g.mapleader = " "
 
-nnoremap("<leader>pv","<cmd>Ex<CR>") -- Goes to netrw
-nnoremap("<leader>p", "<Esc>:w<CR>:!python3 %<CR>") --Run python SC
+vim.keymap.set("n", "<leader>pv","<cmd>Ex<CR>") -- Goes to netrw
+vim.keymap.set("n", "<leader>p", "<Esc>:w<CR>:!python3 %<CR>") --Run python SC
 
 -- Telescope
 --
-nnoremap("<leader>ff","<Esc>:w<CR>:Telescope find_files<CR>")
-nnoremap("<leader>fg","<Esc>:w<CR>:Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>ff","<Esc>:w<CR>:Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fg","<Esc>:w<CR>:Telescope live_grep<CR>")
 
 -- General functionality
 --
 
-nnoremap("<C-h>","<C-w>h") -- change :split naviagtion
-nnoremap("<C-k>","<C-w>k")
-nnoremap("<C-j>","<C-w>j")
-nnoremap("<C-l>","<C-w>l")
+vim.keymap.set("n", "<C-h>","<C-w>h") -- change :split naviagtion
+vim.keymap.set("n", "<C-k>","<C-w>k")
+vim.keymap.set("n", "<C-j>","<C-w>j")
+vim.keymap.set("n", "<C-l>","<C-w>l")
 
 
-nnoremap("<leader>e",":Lex 30<CR>") -- Basically a file explorer (netrw)
+vim.keymap.set("n", "<leader>e",":Lex 30<CR>") -- Basically a file explorer (netrw)
 
-nnoremap("<leader>vs",":vsplit<CR>") --Vertical split
+vim.keymap.set("n", "<leader>vs",":vsplit<CR>") --Vertical split
 
-vnoremap(">",">gv") -- Stay in indent mode
-vnoremap("<","<gv")
+vim.keymap.set("v", ">",">gv") -- Stay in indent mode
+vim.keymap.set("v", "<","<gv")
 
-vnoremap("<A-j>",":m'>+<CR>gv-gv") -- Move text up and down
-vnoremap("<A-k>",":m-2<CR>gv-gv")
+vim.keymap.set("v", "<A-j>",":m'>+<CR>gv-gv") -- Move text up and down
+vim.keymap.set("v", "<A-k>",":m-2<CR>gv-gv")
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Harpoon stuff
 --
 
 local silent = {silent = true}
 
-nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
-nnoremap("<leader>e", function() require("harpoon.ui").toggle_quick_menu() end, silent)
+vim.keymap.set("n", "<leader>a", function() require("harpoon.mark").add_file() end, silent)
+vim.keymap.set("n", "<leader>.", function() require("harpoon.ui").toggle_quick_menu() end, silent)
 
-nnoremap("<leader>h", function() require("harpoon.ui").nav_file(1) end, silent)
-nnoremap("<leader>j", function() require("harpoon.ui").nav_file(2) end, silent)
-nnoremap("<leader>k", function() require("harpoon.ui").nav_file(3) end, silent)
-nnoremap("<leader>l", function() require("harpoon.ui").nav_file(4) end, silent)
+vim.keymap.set("n", "<leader>h", function() require("harpoon.ui").nav_file(1) end, silent)
+vim.keymap.set("n", "<leader>t", function() require("harpoon.ui").nav_file(2) end, silent)
+vim.keymap.set("n", "<leader>n", function() require("harpoon.ui").nav_file(3) end, silent)
+vim.keymap.set("n", "<leader>s", function() require("harpoon.ui").nav_file(4) end, silent)
 
