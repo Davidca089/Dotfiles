5387b342498e1917f2275059fd1f8bc8f5c1bd50
David <david-ortiz.calderon@epita.fr>
2023-01-21 17:10:42 -0500

    ST conf

--- suckless/st_config/config.h
+++ suckless/st_config/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
+static int borderpx = 0;
 
 
 
@@ -94,8 +94,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 4;
+
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.88;
 
 
 /* Terminal colors (16 first used in escape sequence) */
